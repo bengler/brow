1c67ee1143c40b4296e8fc0fcf80af1d89577f4e
Alexander Staubo <alex@origo.no>
2012-04-20 01:40:11 +0200

    Use 'bash -lc' to invoke commands with correct environment, and make sure .rvmrc is trusted so the right Ruby version is used.

--- lib/brow/shell_environment.rb
+++ lib/brow/shell_environment.rb
@@ -5,14 +5,21 @@ module Brow::ShellEnvironment
   # Execute a command in an environment that strive to 
   # support bundler, rbenv and perhaps even rvm
   def self.exec(commands, dir = ENV['HOME'])
-    if File.exists?(ENV['HOME']+"/.rbenv")
-      dir += '/' unless dir[-1] == '/'
+    if File.exist?(File.join(ENV['HOME'], '.rbenv'))
+      dir = "#{dir}/" unless dir =~ /\/$/
       ENV['RBENV_DIR'] = dir 
     end
-    ` . $HOME/.bash_profile
-      cd #{dir} 
-      #{commands}
-    `
+    cmdline = "cd #{dir}"
+    if File.exist?(File.join(dir, '.rvmrc'))
+      # For RVM, we must make sure we trust our .rvmrc, which also requires
+      # reloading RVM
+      cmdline << ' && rvm rvmrc trust'
+      cmdline << ' && rvm reload'
+    end
+    cmdline << ' && ('
+    cmdline << commands.gsub(%("), %(\\"))
+    cmdline << ')'
+    `bash -lc "#{cmdline}"`
   end
 
 end
